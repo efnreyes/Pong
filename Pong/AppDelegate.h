a7705106057fa4909ac32e258c5c8142e3d8635f
Efrén Reyes <efren.reyes@globant.com>
2014-07-31 10:55:46 -0500

    Initial Commit

---
+++ Pong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by Efrén Reyes Torres on 7/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
